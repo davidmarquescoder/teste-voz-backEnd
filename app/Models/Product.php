299843913f8bf47f42fbebd03ab7bc8d67cfefcb
David <davidmarques99@hotmail.com>
2025-01-08 17:50:30 -0300

    Feat: This commit adds relationship between product and category

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Product extends Model
 {
@@ -17,4 +18,13 @@ class Product extends Model
         'description',
         'price',
     ];
+
+    /**
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(related: Category::class);
+    }
 }
