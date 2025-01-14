eb2cfa741f0794b3681f24759f1f128013f22980
David Marques <davidmarques99@hotmail.com>
2025-01-14 19:49:15 -0300

    Feat: This commit adds seeds and factories

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\Category;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Product>
+ */
+class ProductFactory extends Factory
+{
+    protected $model = \App\Models\Product::class;
+
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->word(),
+            'description' => $this->faker->paragraph(),
+            'price' => $this->faker->randomFloat(2, 10, 1000),
+            'category_id' => Category::factory(),
+        ];
+    }
+}
